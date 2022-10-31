a9a82c0cc1fc0c650951a294f0166f19078f2e88
Werterdererste <arslanagica504@gmail.com>
2022-10-31 13:30:09 +0100

    Es können mehrere dateien gleichzeitig audegwählt werden die dateien können vorm uplode noch kontroliert werden. entfernt oder weitere hinzugefügt

--- CloudSpeicher/view/Daschbord/DateiHochladen.Designer.cs
+++ CloudSpeicher/view/Daschbord/DateiHochladen.Designer.cs
@@ -29,47 +29,98 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.buttonFileUploard = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonHinzufügen = new System.Windows.Forms.Button();
+            this.buttonAuswahlLöschen = new System.Windows.Forms.Button();
+            this.buttonHochladen = new System.Windows.Forms.Button();
+            this.buttonAlleLöschen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.8F);
-            this.label1.Location = new System.Drawing.Point(177, 16);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 29);
+            this.label1.Size = new System.Drawing.Size(600, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Datei Hochladen";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // buttonFileUploard
+            // listBox1
             // 
-            this.buttonFileUploard.Location = new System.Drawing.Point(206, 142);
-            this.buttonFileUploard.Name = "buttonFileUploard";
-            this.buttonFileUploard.Size = new System.Drawing.Size(131, 23);
-            this.buttonFileUploard.TabIndex = 2;
-            this.buttonFileUploard.Text = "Datei Auswahl";
-            this.buttonFileUploard.UseVisualStyleBackColor = true;
-            this.buttonFileUploard.Click += new System.EventHandler(this.buttonFileUploard_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(100, 50);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 290);
+            this.listBox1.TabIndex = 3;
+            // 
+            // buttonHinzufügen
+            // 
+            this.buttonHinzufügen.Location = new System.Drawing.Point(100, 350);
+            this.buttonHinzufügen.Name = "buttonHinzufügen";
+            this.buttonHinzufügen.Size = new System.Drawing.Size(75, 23);
+            this.buttonHinzufügen.TabIndex = 4;
+            this.buttonHinzufügen.Text = "Hinzufügen";
+            this.buttonHinzufügen.UseVisualStyleBackColor = true;
+            this.buttonHinzufügen.Click += new System.EventHandler(this.buttonHinzufügen_Click);
+            // 
+            // buttonAuswahlLöschen
+            // 
+            this.buttonAuswahlLöschen.Location = new System.Drawing.Point(200, 350);
+            this.buttonAuswahlLöschen.Name = "buttonAuswahlLöschen";
+            this.buttonAuswahlLöschen.Size = new System.Drawing.Size(100, 23);
+            this.buttonAuswahlLöschen.TabIndex = 5;
+            this.buttonAuswahlLöschen.Text = "Auswahl Löschen";
+            this.buttonAuswahlLöschen.UseVisualStyleBackColor = true;
+            this.buttonAuswahlLöschen.Click += new System.EventHandler(this.buttonAuswahlLöschen_Click);
+            // 
+            // buttonHochladen
+            // 
+            this.buttonHochladen.Location = new System.Drawing.Point(425, 350);
+            this.buttonHochladen.Name = "buttonHochladen";
+            this.buttonHochladen.Size = new System.Drawing.Size(75, 23);
+            this.buttonHochladen.TabIndex = 6;
+            this.buttonHochladen.Text = "Hochladen";
+            this.buttonHochladen.UseVisualStyleBackColor = true;
+            this.buttonHochladen.Click += new System.EventHandler(this.buttonHochladen_Click);
+            // 
+            // buttonAlleLöschen
+            // 
+            this.buttonAlleLöschen.Location = new System.Drawing.Point(325, 350);
+            this.buttonAlleLöschen.Name = "buttonAlleLöschen";
+            this.buttonAlleLöschen.Size = new System.Drawing.Size(75, 23);
+            this.buttonAlleLöschen.TabIndex = 7;
+            this.buttonAlleLöschen.Text = "Alle löschen";
+            this.buttonAlleLöschen.UseVisualStyleBackColor = true;
+            this.buttonAlleLöschen.Click += new System.EventHandler(this.buttonAlleLöschen_Click);
             // 
             // DateiHochladen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.buttonFileUploard);
+            this.Controls.Add(this.buttonAlleLöschen);
+            this.Controls.Add(this.buttonHochladen);
+            this.Controls.Add(this.buttonAuswahlLöschen);
+            this.Controls.Add(this.buttonHinzufügen);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Name = "DateiHochladen";
             this.Size = new System.Drawing.Size(600, 569);
             this.Load += new System.EventHandler(this.DateiHochladen_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonFileUploard;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonHinzufügen;
+        private System.Windows.Forms.Button buttonAuswahlLöschen;
+        private System.Windows.Forms.Button buttonHochladen;
+        private System.Windows.Forms.Button buttonAlleLöschen;
     }
 }
--- CloudSpeicher/view/Daschbord/DateiHochladen.cs
+++ CloudSpeicher/view/Daschbord/DateiHochladen.cs
@@ -13,7 +13,7 @@ namespace CloudSpeicher
 {
     public partial class DateiHochladen : UserControl
     {
-
+        List<Stream> streams = new List<Stream>();
         public DateiHochladen()
         {
             InitializeComponent();
@@ -24,21 +24,56 @@ namespace CloudSpeicher
 
         }
 
-        private void buttonFileUploard_Click(object sender, EventArgs e)
+        private void buttonHinzufügen_Click(object sender, EventArgs e)
         {
             // file auswählen
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
+                openFileDialog.Multiselect = true;
                 openFileDialog.Title = "Hochladen";
                 openFileDialog.InitialDirectory = @"C:\";
 
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                DialogResult result = openFileDialog.ShowDialog();
+                if (result == DialogResult.OK)
                 {
-                    //file in db abspeichern
-                    DatenbankAnbindung db = new DatenbankAnbindung();
-                    db.UplodeFile("Admin", openFileDialog.OpenFile());
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        listBox1.Items.Add(file);
+                        streams.Add(openFileDialog.OpenFile());
+                    }
+                    
                 }
-            }            
+            }
+        }
+
+        private void buttonAuswahlLöschen_Click(object sender, EventArgs e)
+        {
+            int selectet = listBox1.SelectedIndex;
+            if (selectet >= 0)
+            {
+                Console.WriteLine(selectet);
+                streams.RemoveAt(selectet);
+                listBox1.Items.RemoveAt(selectet);
+            }
+           
+        }
+
+        private void buttonAlleLöschen_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            streams.Clear();
+        }
+
+        private void buttonHochladen_Click(object sender, EventArgs e)
+        {
+            ////file in db abspeichern
+            DatenbankAnbindung db = new DatenbankAnbindung();
+            for (int i = 0; i < streams.Count; i++)
+            {
+                db.UplodeFile("Admin", streams[i]);
+            }
+            listBox1.Items.Clear();
+            streams.Clear();
         }
     }
 }
